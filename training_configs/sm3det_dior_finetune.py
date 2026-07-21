acd19dc5de507cbdf1b6d13419f2a7b29ba9e81c
zhangshaoliang <zhangshaoliang@wisdom.cn>
2026-07-21 16:17:37 +0800

    fix: 修复test pipeline的meta_keys, 解决评估时KeyError

--- training_configs/sm3det_dior_finetune.py
+++ training_configs/sm3det_dior_finetune.py
@@ -60,7 +60,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'), dict(type='RResize', img_scale=img_scale),
     dict(type='Normalize', mean=[123.675,116.28,103.53], std=[58.395,57.12,57.375], to_rgb=True),
     dict(type='Pad', size_divisor=32), dict(type='ImageToTensor', keys=['img']),
-    dict(type='Collect', keys=['img'])]
+    dict(type='Collect', keys=['img'], meta_keys=['filename','ori_shape','img_shape','pad_shape','scale_factor'])]
 
 data = dict(
     samples_per_gpu=4, workers_per_gpu=0,
